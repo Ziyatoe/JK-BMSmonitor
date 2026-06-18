012307b545524488dcdd94f9dbb21d9adb113ddf
Ziyatoe <108363609+Ziyatoe@users.noreply.github.com>
2026-06-18 21:04:50 +0300

    Update jkmonitor.4.py

--- jkmonitor.4.py
+++ jkmonitor.4.py
@@ -1,3 +1,23 @@
+"""
+Read/write Data from/to JK_PBxA16S10P and many other newer JK BMS over rs485 on on a pi4 and publish on a MQTT Server
+
+This program is free software: you can redistribute it and/or modify it under the terms of the
+GNU General Public License as published by the Free Software Foundation, either version 3 of the License,
+or any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with this program.
+If not, see <https://www.gnu.org/licenses/>
+
+NO COMMERCIAL USE !!
+
+Copyright 2025 Z.TOe toedci@gmail.com
+------------------------------------------------------------------------------------------------------------------------
+If you do any modification to this python script please SHARE with me, thank you!!!!
+------------------------------------------------------------------------------------------------------------------------
+"""
 import serial
 import struct
 from registers_1_3 import *
